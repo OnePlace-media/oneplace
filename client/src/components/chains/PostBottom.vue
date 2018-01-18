28ae00d6f7faf22647b5ed56bd944883dd0ae40c
rusov.d.s <rusov.d.s@gmail.com>
2018-01-18 17:50:10 +0700

    :speak_no_evil: fix calc

--- client/src/components/chains/PostBottom.vue
+++ client/src/components/chains/PostBottom.vue
@@ -16,7 +16,7 @@
           <svg @click.prevent="$emit('vote', true, voteWeight)" class="post-view__icon post-view__icon-like post-view__icon--disabled">
             <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/static/img/icons-sprite.svg#like"></use>
           </svg>
-          <slider v-if="voteSliderActive && !isLike" :value.sync="voteWeight">
+          <slider v-if="voteSliderActive && !isLike" :value.sync="voteWeight" :max="voteWeightMax">
           </slider>
         </a>{{likeVotes}}
       </span>
@@ -66,7 +66,12 @@ export default {
   },
   computed: {
     showPayoutWithVote() {
-      return this.voteIsSliding && !this.isLike && this.account.username && this.payoutWithVote !== this.post.payout
+      return (
+        this.voteIsSliding &&
+        !this.isLike &&
+        this.account.username &&
+        this.payoutWithVote !== this.post.payout
+      )
     },
     voteSliderActive() {
       return (
@@ -84,41 +89,27 @@ export default {
     },
     payoutWithVote() {
       const CONSTANT_S = 2000000000000
+      const STEEMIT_VOTE_REGENERATION_SECONDS = 5 * 60 * 60 * 24
+      const STEEM_100_PERCENT = 10000
+      const VOTE_POWER_REVERSE_RATE = this.$store.state.params[this.chain].globalProps.vote_power_reserve_rate || 40
       const CURRENCY = {
         [CONSTANTS.BLOCKCHAIN.SOURCE.STEEM]: 1,
         [CONSTANTS.BLOCKCHAIN.SOURCE.GOLOS]: this.$store.state.params[
           CONSTANTS.BLOCKCHAIN.SOURCE.GOLOS
         ].goldPrice
       }
-      const steem_per_mvests = this.$store.state.params[this.chain]
-        .steem_per_mvests
-
-      const vestsToSp = vests => {
-        return vests / 1e6 * steem_per_mvests
-      }
-
-      const spToVests = sp => {
-        return sp * 1e6 / steem_per_mvests
-      }
-
-      const vestsToRshares = (sp, voting_power = 10000, vote_pct = 10000) => {
-        const vesting_shares = parseInt(spToVests(sp) * 1e6, 10)
-        const power = voting_power * vote_pct / 10000 / 200 + 1
-        return power * vesting_shares / 10000
-      }
+      const steem_per_mvests = this.$store.state.params[this.chain].steem_per_mvests
 
       const calculateVshares = rshares => {
-        return (
-          (rshares + CONSTANT_S) * (rshares + CONSTANT_S) -
-          CONSTANT_S * CONSTANT_S
-        )
+        return (Math.pow(rshares + CONSTANT_S, 2) - Math.pow(CONSTANT_S, 2))
       }
 
-      const data = this.account.data
       const { recent_claims, reward_balance } = this.$store.state.params[
         this.chain
       ].rewardFunds
 
+      const data = this.account.data
+      
       const voting_power = data.voting_power
       let total_vests = parseFloat(data.vesting_shares.split(' ')[0])
 
@@ -128,9 +119,15 @@ export default {
       if (data.delegated_vesting_shares)
         total_vests -= parseFloat(data.delegated_vesting_shares.split(' ')[0])
 
-      const sp = vestsToSp(total_vests)
       const vote_pct = this.voteWeight
-      const rshares = vestsToRshares(sp, voting_power, vote_pct)
+
+
+      const max_vote_denom = VOTE_POWER_REVERSE_RATE * STEEMIT_VOTE_REGENERATION_SECONDS / (60*60*24)
+      let used_power = (voting_power * vote_pct) / STEEM_100_PERCENT
+      used_power = (used_power + max_vote_denom - 1) / max_vote_denom
+
+      let rshares = total_vests * used_power / STEEM_100_PERCENT
+      rshares = Math.floor(rshares * 1000000)
       const feedPrice = this.$store.state.params[this.chain].feedPrice
       const base =
         parseFloat(feedPrice.base.split(' ')[0]) /

583355b622b9907116c90a14c55dfd530d345d75
rusov.d.s <rusov.d.s@gmail.com>
2018-02-13 15:51:44 +0700

    hide post create

--- client/src/containers/chains/PostCreate.vue
+++ client/src/containers/chains/PostCreate.vue
@@ -1,51 +1,51 @@
 <template>
   <section class="main-content">
-    <div class="container">
+    <!-- <div class="container">
       <no-ssr>
         <markdown-editor v-model="content" ref="markdownEditor" :configs="configs"></markdown-editor>
       </no-ssr>
-    </div>
+    </div> -->
   </section>
 </template>
 
 <style>
-@import '~simplemde/dist/simplemde.min.css';
+/* @import '~simplemde/dist/simplemde.min.css'; */
 </style>
 
 <script>
-export default {
-  name: 'PostCreate',
-  data() {
-    return {
-      content: '',
-      configs: {
-        toolbar: [
-          {
-            name: 'bold',
-            action: () => {},
-            className: 'fa fa-bold',
-            title: 'Bold'
-          },
-          {
-            name: 'custom',
-            action: function customFunction(editor) {
-              // Add your own code
-            },
-            className: 'fa fa-star',
-            title: 'Custom Button'
-          },
-          '|' // Separatorz`
-        ]
-      }
-    }
-  },
-  mounted() {
-    setTimeout(() => {
-      const bar = document.getElementsByClassName('editor-toolbar')[0]
-      bar.parentNode.removeChild(bar)
-      this.configs.toolbar[0].title = 'Русский'
-      this.$refs.markdownEditor.simplemde.createToolbar(this.configs.toolbar)
-    }, 1000)
-  }
-}
+// export default {
+//   name: 'PostCreate',
+//   data() {
+//     return {
+//       content: '',
+//       configs: {
+//         toolbar: [
+//           {
+//             name: 'bold',
+//             action: () => {},
+//             className: 'fa fa-bold',
+//             title: 'Bold'
+//           },
+//           {
+//             name: 'custom',
+//             action: function customFunction(editor) {
+//               // Add your own code
+//             },
+//             className: 'fa fa-star',
+//             title: 'Custom Button'
+//           },
+//           '|' // Separatorz`
+//         ]
+//       }
+//     }
+//   },
+//   mounted() {
+//     setTimeout(() => {
+//       const bar = document.getElementsByClassName('editor-toolbar')[0]
+//       bar.parentNode.removeChild(bar)
+//       this.configs.toolbar[0].title = 'Русский'
+//       this.$refs.markdownEditor.simplemde.createToolbar(this.configs.toolbar)
+//     }, 1000)
+//   }
+// }
 </script>
